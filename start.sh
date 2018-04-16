c4869eb6e9895eb28012d9d11797d40cf3eb438f
Sir_Boops <admin@boops.me>
2018-04-16 06:43:51 -0600

    Removed uneeded crap

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
 #!/bin/ash
-
-MODE=`printenv RUN_MODE`
-
-if [ $MODE == "PROD" ]; then
-    su - -s /bin/ash pleroma -c 'cd pleroma && MIX_ENV=prod mix ecto.migrate'
-    su - -s /bin/ash pleroma -c 'cd pleroma && MIX_ENV=prod mix phx.server'
-else
-    su - -s /bin/ash pleroma -c 'cd pleroma && mix ecto.migrate'
-    su - -s /bin/ash pleroma -c 'cd pleroma && mix phx.server'
-fi
+su - -s /bin/ash pleroma -c 'cd pleroma && mix ecto.migrate'
+su - -s /bin/ash pleroma -c 'cd pleroma && mix phx.server'
