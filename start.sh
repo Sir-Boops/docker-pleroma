00b4d65de03f61458f6326a1a79e83561f105df1
Sir_Boops <admin@boops.me>
2018-04-16 05:46:34 -0600

    Add running in prod mode

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/ash
-su - -s /bin/ash pleroma -c 'cd pleroma && mix ecto.migrate'
-su - -s /bin/ash pleroma -c 'cd pleroma && mix phx.server'
+
+MODE=`printenv RUN_MODE`
+
+if [ $MODE == "PROD" ]; then
+    su - -s /bin/ash pleroma -c 'cd pleroma && MIX_ENV=prod mix ecto.migrate'
+    su - -s /bin/ash pleroma -c 'cd pleroma && MIX_ENV=prod mix phx.server'
+else
+    su - -s /bin/ash pleroma -c 'cd pleroma && mix ecto.migrate'
+    su - -s /bin/ash pleroma -c 'cd pleroma && mix phx.server'
+fi
